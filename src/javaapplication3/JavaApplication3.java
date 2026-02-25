f15f0ec44ab02c1a0cbdd6582eaa4de2e5427e58
ST10527558 <namdizzi8765@gmail.com>
2026-02-25 09:07:41 +0200

    part1

---
+++ src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication3;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ST10527558
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //System.out.println("hello\nwelcome\nto\nthis\rworld");
+        //JOptionPane.showMessageDialog(null, "hello\nwelcome\nto\nthis\bworld");
+        
+        /**
+         * stop and go sign
+         */
+        
+        System.out.println("\t\t  ********\t\tXXXXXXXX"
+                + "\n\t\t*\t   *" + "\t      X\t\t X"
+                + "\n\t      *\t    STOP     *" + "\t    X\t   GO      X"
+                + "\n\t\t*\t   *" + "\t      X\t         X"
+                + "\n\t\t  ********\t\tXXXXXXXX\n\t\t     **\t\t\t   XX\n\t\t"
+                + "     **\t\t\t   XX\n\t\t     **\t\t\t   XX\n\t\t     "
+                + "**\t\t\t   XX" + "\n\t\t    ****\t\t  XXXX\n\t\t  ********"
+                + "\t\tXXXXXXXX");
+        
+        
+
+    }
+}
